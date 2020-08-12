6a8e739cdbc60a3413bf7864b03e56b3bfb1466c
Steven Vandegrift <svandegrift@icloud.com>
2020-08-11 23:37:02 -0500

    Add Share Functionality

--- Project1/DetailViewController.swift
+++ Project1/DetailViewController.swift
@@ -21,6 +21,18 @@ class DetailViewController: UIViewController {
         }
         title = newTitle ?? selectedImage
         navigationItem.largeTitleDisplayMode = .never
+        navigationItem.rightBarButtonItem = UIBarButtonItem(barButtonSystemItem: .action, target: self, action: #selector(shareTapped))
+    }
+    
+    @objc func shareTapped(){
+        guard let image = imageView.image?.jpegData(compressionQuality: 1) else {
+            print("No image found")
+            return
+        }
+        
+        let vc = UIActivityViewController(activityItems: [image], applicationActivities: [])
+        vc.popoverPresentationController?.barButtonItem = navigationItem.rightBarButtonItem
+        present(vc, animated: true)
     }
     
     override func viewWillAppear(_ animated: Bool) {
